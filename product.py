1a37e2e2d8b6f73ada8f564606c26f529e6331a9
sayeesh <sayeeshpatil64@gmail.com>
2026-01-06 11:49:57 +0530

    save

---
+++ product.py
@@ -0,0 +1,16 @@
+import pytest
+def product_details(name, id, quantity, price):
+    result = (
+        f"Product Name: {name}\n"
+        f"Product ID: {id}\n"
+        f"Quantity: {quantity}\n"
+        f"Price: {price}"
+    )
+    return result
+if _name_ == "_main_":
+    #Sample Output
+    name = "Lenovo Laptop"
+    id = "P1001"
+    quantity = 10
+    price = 48000
+    print(product_details(name, id, quantity, price))
